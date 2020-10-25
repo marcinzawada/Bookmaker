95f9bcd8cfa9ebddbdb09a92fa6d5ad79680a658
Marcin <ziewa91@wp.pl>
2020-10-25 23:18:38 +0100

    Added dto to models mappers

---
+++ ApiFootball/Mappers/IDtoToModelMapper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApiFootball.Mappers
+{
+    public interface IDtoToModelMapper<in TDto, out TModel>
+    {
+        public TModel MapDtoToModel(TDto dto);
+    }
+}
---
+++ ApiFootball/Mappers/LabelMapper.cs
@@ -0,0 +1,25 @@
+﻿using Api.Data.Models;
+using ApiFootball.DTOs.Labels;
+using Bookmaker.Api.Data.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApiFootball.Mappers
+{
+    class LabelMapper : BaseMapper, IDtoToModelMapper<LabelDto, Label>
+    {
+        public LabelMapper(AppDbContext context) : base(context)
+        {
+        }
+
+        public Label MapDtoToModel(LabelDto dto)
+        {
+            return new Label
+            {
+                ExtLabelId = dto.Id,
+                Name = dto.Name
+            };
+        }
+    }
+}
