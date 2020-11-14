c180f09de5ac233c6321664eb6379993b34c22fe
Marcin <ziewa91@wp.pl>
2020-11-14 18:24:39 +0100

    Added Coupon controller

---
+++ Bookmaker/Controllers/CouponController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bookmaker.Requests.Coupon;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookmaker.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class CouponController : ControllerBase
+    {
+        public IActionResult MakeCoupon(MakeCouponRequest request)
+        {
+            return Ok();
+        }
+    }
+}
