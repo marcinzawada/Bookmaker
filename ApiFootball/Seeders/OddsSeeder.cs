1ad164d0e03669383de97418d21565cc93fd94de
Marcin <ziewa91@wp.pl>
2021-06-01 19:23:11 +0200

    Added OddsSeeder

---
+++ ApiFootball/Seeders/OddsSeeder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ApiFootball.Client;
+using Infrastructure.Data;
+
+namespace ApiFootball.Seeders
+{
+    public class OddsSeeder : BaseSeeder
+    {
+        public OddsSeeder(AppDbContext context, IApiFootballClient client) : base(context, client)
+        {
+        }
+
+        public async Task SeedOdds(int extLeagueId)
+        {
+            var odds = await _client.DownloadAllOddsByLeagueId(extLeagueId);
+            await _context.FixtureOdds.AddRangeAsync(odds);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
