04c422f38765dbbcfdc2ef378352ea1eb8bac448
Marcin <ziewa91@wp.pl>
2021-06-04 18:14:44 +0200

    Added IUserRepository

---
+++ Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Domain.Entities;
+
+namespace Domain.Repositories
+{
+    public interface IUserRepository : IGenericRepository<User>
+    {
+        Task<User> GetSingleByEmail(string email, CancellationToken cancellationToken);
+    }
+}
