8b9160ed26eae986db51d8663584269a513566f6
Marcin <ziewa91@wp.pl>
2021-06-04 18:17:05 +0200

    Added AuthenticationAppConfig

---
+++ WebAPi/AppConfigs/AuthenticationAppConfig.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using Application.Common.JWT;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+namespace WebAPI.AppConfigs
+{
+    public static class AuthenticationAppConfig
+    {
+        public static void AddAuthentication(this IServiceCollection services, IConfiguration configuration)
+        {
+            var authenticationSettings = new AuthenticationSettings();
+
+            configuration.GetSection("Authentication").Bind(authenticationSettings);
+            services.AddSingleton(authenticationSettings);
+
+            services.AddAuthentication(option =>
+            {
+                option.DefaultAuthenticateScheme = "Bearer";
+                option.DefaultScheme = "Bearer";
+                option.DefaultChallengeScheme = "Bearer";
+            }).AddJwtBearer(cfg =>
+            {
+                cfg.RequireHttpsMetadata = false;
+                cfg.SaveToken = true;
+                cfg.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(authenticationSettings.JwtKey)),
+                };
+            });
+        }
+    }
+}
