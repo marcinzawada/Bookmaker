8c7eeb735c902fa730b91ab0a6909bbbc0fd5546
Marcin <ziewa91@wp.pl>
2021-06-04 18:02:21 +0200

    Added LoginCommand

---
+++ Application/Commands/Account/Login/LoginCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Models;
+using FluentValidation;
+using MediatR;
+
+namespace Application.Commands.Account.Login
+{
+    public record LoginCommand(string Email, string Password) : IRequest<Response>;
+
+    public class LoginCommandValidator : AbstractValidator<LoginCommand>
+    {
+        public LoginCommandValidator()
+        {
+            RuleFor(x => x.Email).NotNull();
+            RuleFor(x => x.Password).NotNull();
+        }
+    }
+}
