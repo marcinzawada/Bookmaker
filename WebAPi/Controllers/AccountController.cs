d7a061eb66d76b616c23a6d6255bc1115c0fd21d
Marcin <ziewa91@wp.pl>
2021-06-04 18:17:34 +0200

    Added AccountController

---
+++ WebAPi/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Commands;
+using Application.Commands.Account.Login;
+using Application.Commands.Account.Register;
+using Application.Extensions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    public class AccountController : ApiControllerBase
+    {
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterCommand command)
+        {
+            var response = await Mediator.Send(command);
+
+            return this.CreateResponse(response);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginCommand command)
+        {
+            var response = await Mediator.Send(command);
+
+            return this.CreateResponse(response);
+        }
+    }
+}
