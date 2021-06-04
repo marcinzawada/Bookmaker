0dc0c8d30895e17819a08072faaccb91d1129910
Marcin <ziewa91@wp.pl>
2021-06-04 18:17:19 +0200

    Added SwaggerAppConfig

---
+++ WebAPi/AppConfigs/SwaggerAppConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using NSwag;
+using NSwag.Generation.Processors.Security;
+
+namespace WebAPI.AppConfigs
+{
+    public static class SwaggerAppConfig
+    {
+        public static IServiceCollection ConfigureSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerDocument(document =>
+            {
+                document.Title = "Bookmaker App Documentation";
+                document.DocumentName = "swagger";
+                document.OperationProcessors.Add(new OperationSecurityScopeProcessor("jwt"));
+                document.DocumentProcessors.Add(new SecurityDefinitionAppender("jwt", new OpenApiSecurityScheme
+                {
+                    Type = OpenApiSecuritySchemeType.ApiKey,
+                    Name = "Authorization",
+                    In = OpenApiSecurityApiKeyLocation.Header,
+                    Description = "JWT Token - remember to add 'Bearer ' before the token",
+                }));
+            });
+
+            return services;
+        }
+    }
+}
