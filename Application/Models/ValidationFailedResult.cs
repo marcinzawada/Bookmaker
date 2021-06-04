67340e6e7179c1ca5fcf89aeb6005b29c89ccd55
Marcin <ziewa91@wp.pl>
2021-06-04 18:09:39 +0200

    Added ValidationFailedResult

---
+++ Application/Models/ValidationFailedResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Application.Models
+{
+    public class ValidationFailedResult : ObjectResult
+    {
+        public ValidationFailedResult(ModelStateDictionary modelState)
+            : base(ValidationResponse.Create(modelState))
+        {
+            StatusCode = StatusCodes.Status422UnprocessableEntity;
+        }
+    }
+}
