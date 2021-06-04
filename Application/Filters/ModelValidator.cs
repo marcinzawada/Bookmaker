90279e30c47c27ceb3cd8ee7a5b82565520a424b
Marcin <ziewa91@wp.pl>
2021-06-04 18:07:30 +0200

    AddedModelValidator

---
+++ Application/Filters/ModelValidator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Common.Constants;
+using Application.Models;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Application.Filters
+{
+    public class ModelValidatorFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                context.Result = new ValidationFailedResult(context.ModelState);
+            }
+
+        }
+    }
+}
