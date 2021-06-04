e6af0b615b0151ff162392f94802a68900b60f9c
Marcin <ziewa91@wp.pl>
2021-06-04 18:03:10 +0200

    Added RegisterCommandHandler

---
+++ Application/Commands/Account/Register/RegisterCommandHandler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Models;
+using Domain.Entities;
+using Domain.Repositories;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+
+namespace Application.Commands.Account.Register
+{
+    public class RegisterCommandHandler : IRequestHandler<RegisterCommand, Response>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IPasswordHasher<User> _passwordHasher;
+
+
+        public RegisterCommandHandler(IUserRepository userRepository, IPasswordHasher<User> passwordHasher)
+        {
+            _userRepository = userRepository;
+            _passwordHasher = passwordHasher;
+        }
+
+        public async Task<Response> Handle(RegisterCommand request, CancellationToken cancellationToken)
+        {
+            var newUser = new User()
+            {
+                Email = request.Email,
+            };
+
+            var hashedPassword = _passwordHasher.HashPassword(newUser, request.Password);
+            newUser.PasswordHash = hashedPassword;
+
+            await _userRepository.AddAsync(newUser, cancellationToken);
+
+            return Response.Success();
+        }
+    }
+}
