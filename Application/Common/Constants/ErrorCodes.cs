d31b3215bc4de6b085bb9548480957591bbb1b7a
Marcin <ziewa91@wp.pl>
2021-06-04 18:03:43 +0200

    Added ErrorCodes

---
+++ Application/Common/Constants/ErrorCodes.cs
@@ -0,0 +1,15 @@
+﻿namespace Application.Common.Constants
+{
+    public static class ErrorCodes
+    {
+        public const string ServerError = "SERVER_ERROR";
+        public const string CrudActionFailed = "CRUD_ACTION_FAILED";
+        public const string ValidationError = "VALIDATION_ERROR";
+        public const string DatabaseError = "DATABASE_ERROR";
+        public const string ServiceError = "SERVICE_UNAVAILABLE";
+        public const string EntityNotFound = "ENTITY_NOT_FOUND";
+        public const string AccessDenied = "ACCESS_DENIED";
+        public const string AuthError = "AUTHENTICATION_ERROR";
+        public const string InvalidCredentials = "INVALID_CREDENTIALS";
+    }
+}
