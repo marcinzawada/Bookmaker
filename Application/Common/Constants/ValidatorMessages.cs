401355aa9459d1c8939291af593bcc81cdb5f527
Marcin <ziewa91@wp.pl>
2021-06-04 18:04:25 +0200

    Added ValidatorMessages

---
+++ Application/Common/Constants/ValidatorMessages.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Common.Constants
+{
+    public static class ValidatorMessages
+    {
+        public const string ModelValidatorMessage = "Invalid input data";
+
+    }
+}
