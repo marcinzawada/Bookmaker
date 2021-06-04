91724900fed43f3bd5963f0456dab0b4182d16a2
Marcin <ziewa91@wp.pl>
2021-06-04 18:08:52 +0200

    Added Response

---
+++ Application/Models/Response.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Application.Models
+{
+    public class Response
+    {
+        public bool IsSucceeded { get; init; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public Error Error { get; init; }
+
+        [System.Text.Json.Serialization.JsonIgnore]
+        public HttpStatusCode HttpStatus { get; init; }
+
+        protected Response(bool isSucceeded, Error error, HttpStatusCode httpStatus = HttpStatusCode.OK)
+        {
+            IsSucceeded = isSucceeded;
+            Error = error;
+            HttpStatus = error?.StatusCode ?? httpStatus;
+        }
+
+        public static Response Success(HttpStatusCode statusCode = HttpStatusCode.OK)
+        {
+            return new(true, null, statusCode);
+        }
+
+        public static Response Failure(Error error)
+        {
+            return new(false, error, HttpStatusCode.BadRequest);
+        }
+    }
+}
