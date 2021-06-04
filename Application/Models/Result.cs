2ebbddf771c3e7837ede3f9195f3f3803632bddc
Marcin <ziewa91@wp.pl>
2021-06-04 18:09:11 +0200

    Added Result

---
+++ Application/Models/Result.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Models
+{
+    public class Result<T> : Response
+    {
+        public T Content { get; init; }
+
+        protected Result(T content, bool isSucceeded, Error error, HttpStatusCode httpStatus = HttpStatusCode.OK) : base(isSucceeded, error, httpStatus)
+        {
+            Content = content;
+        }
+
+        public static Result<T> Create(T content, HttpStatusCode statusCode = HttpStatusCode.OK)
+        {
+            return new(content, true, null, statusCode);
+        }
+    }
+}
