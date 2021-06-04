73e5a7280e83f7ef3db9b22f0b22ecd0ab078bbc
Marcin <ziewa91@wp.pl>
2021-06-04 18:09:54 +0200

    Added ValidationResponse

---
+++ Application/Models/ValidationResponse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Common.Constants;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Application.Models
+{
+    public class ValidationResponse : Response
+    {
+        public List<ValidationError> ValidationErrors { get; init; }
+
+        protected ValidationResponse(ModelStateDictionary modelState, bool isSucceeded, Error error, HttpStatusCode httpStatus = HttpStatusCode.OK) : base(isSucceeded, error, httpStatus)
+        {
+            ValidationErrors = (modelState.Keys.SelectMany(key =>
+                modelState[key].Errors.Select(e =>
+                    new ValidationError(key, e.ErrorMessage))).ToList());
+        }
+
+        public static ValidationResponse Create(ModelStateDictionary modelState)
+        {
+            return new(modelState, false,
+                Errors.ModelValidatorError);
+        }
+    }
+}
