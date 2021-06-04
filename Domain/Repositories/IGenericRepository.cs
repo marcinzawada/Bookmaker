69985da746985d35fee12a6aed94b71f60ddd51f
Marcin <ziewa91@wp.pl>
2021-06-04 18:14:30 +0200

    Added IGenericRepository

---
+++ Domain/Repositories/IGenericRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Domain.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id, CancellationToken cancellationToken);
+        Task<IEnumerable<T>> GetAllAsync(CancellationToken cancellationToken);
+        Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> expression, CancellationToken cancellationToken);
+        Task AddAsync(T entity, CancellationToken cancellationToken);
+        Task AddRangeAsync(IEnumerable<T> entities, CancellationToken cancellationToken);
+        Task RemoveAsync(T entity, CancellationToken cancellationToken);
+        Task RemoveRangeAsync(IEnumerable<T> entities, CancellationToken cancellationToken);
+    }
+}
