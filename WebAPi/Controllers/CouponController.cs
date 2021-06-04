8013fd292dcf0d052b186c4695c056711996cb7c
Marcin <ziewa91@wp.pl>
2021-06-04 18:18:36 +0200

    Changed namespace, deleted unused endpoint

--- WebAPi/Controllers/CouponController.cs
+++ WebAPi/Controllers/CouponController.cs
@@ -1,15 +1,10 @@
-﻿using API.Requests.Coupon;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
-namespace API.Controllers
+namespace WebAPI.Controllers
 {
     [ApiController]
     [Route("[controller]/[action]")]
     public class CouponController : ControllerBase
     {
-        public IActionResult MakeCoupon(MakeCouponRequest request)
-        {
-            return Ok();
-        }
     }
 }
