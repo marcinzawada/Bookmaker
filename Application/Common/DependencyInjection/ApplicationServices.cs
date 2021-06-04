69eb4d382ddbf4ccf395b75b9e0cdbcb26bbd4a3
Marcin <ziewa91@wp.pl>
2021-06-04 18:04:46 +0200

    Added ApplicationServices

---
+++ Application/Common/DependencyInjection/ApplicationServices.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using Application.Common.JWT;
+using Domain.Entities;
+using FluentValidation;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application.Common.DependencyInjection
+{
+    public static class ApplicationServices
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            //services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
+            services.AddHttpContextAccessor();
+
+            services.AddScoped<IJwtGenerator, JwtGenerator>();
+
+            return services;
+        }
+    }
+}
