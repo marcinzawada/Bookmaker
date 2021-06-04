5edc73f72d040eb0ded87ab0cf196a3c1ba45e28
Marcin <ziewa91@wp.pl>
2021-06-04 18:02:44 +0200

    Added LoginDto

---
+++ Application/Commands/Account/Login/LoginDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Commands.Account.Login
+{
+    public class LoginDto
+    {
+        public int Id { get; init; }
+
+        public string Token { get; init; }
+    }
+}
