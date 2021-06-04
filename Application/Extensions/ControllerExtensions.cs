74da32833c8ca817840bd46ec03556c05230e8c6
Marcin <ziewa91@wp.pl>
2021-06-04 18:08:40 +0200

    Added ControllerExtensions

---
+++ Application/Extensions/ControllerExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Application.Extensions
+{
+    public static class ControllerExtensions
+    {
+        public static IActionResult CreateResponse(this ControllerBase controller, Response response)
+            => controller.StatusCode((int)response.HttpStatus, response);
+
+    }
+}
