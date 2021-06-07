2164cad5b028f6842f2841c9576b2dc8e40bd0d5
Marcin <ziewa91@wp.pl>
2021-06-07 12:42:17 +0200

    Added BasePaginatedQuery

---
+++ Application/Models/BasePaginatedQuery.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Models
+{
+    public record BasePaginatedQuery(int ItemPerPage = 10, int PageNumber = 1);
+}
