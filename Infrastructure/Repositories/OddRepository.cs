8e94168d6f1da551f7de8f76e4e8ee2f39f56e83
Marcin <ziewa91@wp.pl>
2021-06-07 12:48:49 +0200

    Added OddRepository

---
+++ Infrastructure/Repositories/OddRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+using Domain.Entities;
+using Domain.Repositories;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+    public class OddRepository : GenericRepository<Odd>, IOddRepository
+    {
+        public OddRepository(AppDbContext context) : base(context)
+        {
+        }
+
+        public async Task<List<Odd>> GetOddsWithFixturesAndTeamsByLeagueIdAsync(int leagueId, CancellationToken cancellationToken)
+        {
+            return await _context.Odds
+                .Include(x => x.Fixture)
+                .ThenInclude(x => x.AwayTeam)
+                .Include(x => x.Fixture)
+                .ThenInclude(x => x.HomeTeam)
+                .Where(x => x.LeagueId == leagueId)
+                .ToListAsync(cancellationToken);
+        }
+    }
+}
