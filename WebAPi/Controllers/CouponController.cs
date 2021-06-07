af2d5f0c94b3ef9de532bbac10035fbd88f26f6f
Marcin <ziewa91@wp.pl>
2021-06-07 12:52:41 +0200

    Added Post method, CouponController now inherits from ApiControllerBase

--- WebAPi/Controllers/CouponController.cs
+++ WebAPi/Controllers/CouponController.cs
@@ -1,10 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using Application.Commands.Coupons;
+using Application.Extensions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WebAPI.Controllers
 {
-    [ApiController]
-    [Route("[controller]/[action]")]
-    public class CouponController : ControllerBase
+    public class CouponController : ApiControllerBase
     {
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> Post(CreateCouponCommand request)
+        {
+            var response = await Mediator.Send(request);
+
+            return this.CreateResponse(response);
+        }
     }
 }
