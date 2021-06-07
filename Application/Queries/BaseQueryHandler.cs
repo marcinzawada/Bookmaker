d0392e992053fd0c29b3da906681ae05ddfa4d05
Marcin <ziewa91@wp.pl>
2021-06-07 12:43:29 +0200

    Added BaseQueryHandler

---
+++ Application/Queries/BaseQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Common.Interfaces;
+using AutoMapper;
+
+namespace Application.Queries
+{
+    public abstract class BaseQueryHandler
+    {
+        protected readonly IDbContext _context;
+        protected readonly IMapper _mapper;
+
+        protected BaseQueryHandler(IDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+    }
+}
