9ca144a9d4d28558d5c0359d6910bbc7022093d6
Marcin <ziewa91@wp.pl>
2021-06-07 12:36:19 +0200

    Added Automapper

--- Application/Common/DependencyInjection/ApplicationServices.cs
+++ Application/Common/DependencyInjection/ApplicationServices.cs
@@ -12,12 +12,13 @@ namespace Application.Common.DependencyInjection
     {
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
-            //services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
             services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
             services.AddMediatR(Assembly.GetExecutingAssembly());
 
             services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
             services.AddHttpContextAccessor();
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
 
             services.AddScoped<IJwtGenerator, JwtGenerator>();
 
