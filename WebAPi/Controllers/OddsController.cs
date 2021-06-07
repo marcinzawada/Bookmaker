07e8079a2bd28636e1195d2153ffa045a4bb5b87
Marcin <ziewa91@wp.pl>
2021-06-07 12:52:53 +0200

    Added OddsController

---
+++ WebAPi/Controllers/OddsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Commands.Account.Register;
+using Application.Extensions;
+using Application.Queries.Odds;
+using Application.Queries.Odds.GetOdds;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using YamlDotNet.Core.Events;
+
+namespace WebAPI.Controllers
+{
+    public class OddsController : ApiControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get([FromQuery]GetOddsQuery query)
+        {
+            var response = await Mediator.Send(query);
+
+            return this.CreateResponse(response);
+        }
+    }
+}
