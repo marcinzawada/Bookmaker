5f2b89f7b8474f7c9762074e8a0be0e06edcde42
Marcin <ziewa91@wp.pl>
2021-06-07 12:50:25 +0200

    Added BetsController

---
+++ WebAPi/Controllers/BetsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Extensions;
+using Application.Queries.Bets.GetBets;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    public class BetsController : ApiControllerBase
+    {
+
+        [HttpGet]
+        public async Task<IActionResult> Get([FromQuery]GetBetsQuery request)
+        {
+            var response = await Mediator.Send(request);
+
+            return this.CreateResponse(response);
+        }
+    }
+}
