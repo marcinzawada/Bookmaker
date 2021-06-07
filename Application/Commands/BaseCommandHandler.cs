18f55529c0d307f1c5cbb2ab7ecdb6bd3cd6ce08
Marcin <ziewa91@wp.pl>
2021-06-07 12:34:32 +0200

    Added BaseCommandHandler

---
+++ Application/Commands/BaseCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Common.Interfaces;
+using Application.Services;
+
+namespace Application.Commands
+{
+    public abstract class BaseCommandHandler
+    {
+        protected readonly IDbContext _context;
+        protected readonly IUserContextService _userContextService;
+
+        protected BaseCommandHandler(IDbContext context, IUserContextService userContextService)
+        {
+            _context = context;
+            _userContextService = userContextService;
+        }
+    }
+}
