7b2b1317460dcaaf7ba1022b293970b0bfcb09c1
Marcin <ziewa91@wp.pl>
2021-06-07 12:42:29 +0200

    Added CreatedResponse

---
+++ Application/Models/CreatedResponse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Application.Models
+{
+    public class CreatedResponse : Response
+    {
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public string ResourcePath { get; init; }
+
+        protected CreatedResponse(string resourcePath, bool isSucceeded, Error error, HttpStatusCode httpStatus = HttpStatusCode.OK) : base(isSucceeded, error, httpStatus)
+        {
+            ResourcePath = resourcePath;
+        }
+
+        public static CreatedResponse Create(string resourcePath)
+        {
+            return new CreatedResponse(resourcePath, true, null, HttpStatusCode.Created);
+        }
+    }
+}
