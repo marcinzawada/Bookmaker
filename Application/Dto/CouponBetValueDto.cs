12d51b8850cb01e52eac1bd9d378c8716a7d1c3d
Marcin <ziewa91@wp.pl>
2021-06-11 22:52:31 +0200

    Added CouponBetValueDto

---
+++ Application/Dto/CouponBetValueDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Common.Mappings;
+using Domain.Entities;
+
+namespace Application.Dto
+{
+    public class CouponBetValueDto : IMapFrom<CouponBetValue>
+    {
+        public int CouponId { get; set; }
+
+        public CouponDto Coupon { get; set; }
+
+        public int BetValueId { get; set; }
+
+        public BetValueDto BetValue { get; set; }
+    }
+}
