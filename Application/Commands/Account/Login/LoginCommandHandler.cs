503a7f582e7166f08125437d234622098a14d6ab
Marcin <ziewa91@wp.pl>
2021-08-24 17:33:49 +0200

    Changed repository to context

--- Application/Commands/Account/Login/LoginCommandHandler.cs
+++ Application/Commands/Account/Login/LoginCommandHandler.cs
@@ -6,33 +6,43 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Application.Common.Constants;
+using Application.Common.Interfaces;
 using Application.Common.JWT;
 using Application.Models;
+using Application.Services;
 using Domain.Entities;
 using Domain.Repositories;
 using MediatR;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application.Commands.Account.Login
 {
-    public class LoginCommandHandler : IRequestHandler<LoginCommand, Response>
+    public class LoginCommandHandler : BaseCommandHandler, IRequestHandler<LoginCommand, Response>
     {
-        private readonly IUserRepository _userRepository;
         private readonly IPasswordHasher<User> _passwordHasher;
         private readonly IJwtGenerator _jwtGenerator;
 
-
-        public LoginCommandHandler(IUserRepository userRepository, IPasswordHasher<User> passwordHasher, IJwtGenerator jwtGenerator)
+        public LoginCommandHandler(IDbContext context, IUserContextService userContextService, IPasswordHasher<User> passwordHasher, IJwtGenerator jwtGenerator) : base(context, userContextService)
         {
-            _userRepository = userRepository;
             _passwordHasher = passwordHasher;
             _jwtGenerator = jwtGenerator;
         }
 
+
+        //public LoginCommandHandler(IUserRepository userRepository, IPasswordHasher<User> passwordHasher, IJwtGenerator jwtGenerator)
+        //{
+        //    _userRepository = userRepository;
+        //    _passwordHasher = passwordHasher;
+        //    _jwtGenerator = jwtGenerator;
+        //}
+
         public async Task<Response> Handle(LoginCommand request, CancellationToken cancellationToken)
         {
-            var user = await _userRepository
-                .GetSingleByEmail(request.Email, cancellationToken);
+            var user = await _context.Users
+                .FirstOrDefaultAsync(x => x.Email == request.Email, cancellationToken);
+
+            var xd = await _context.Users.ToListAsync(cancellationToken);
 
             if (user is null)
                 return Response.Failure(Errors.InvalidCredentials);
