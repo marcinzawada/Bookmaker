7c33bd3294cbabb592db9ff58542c3df18e8470d
Marcin <ziewa91@wp.pl>
2021-08-24 17:30:55 +0200

    Changed odds to bets

---
+++ ApiFootball/Seeders/BetsSeeder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ApiFootball.Client;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiFootball.Seeders
+{
+    public class BetsSeeder : BaseSeeder
+    {
+        public BetsSeeder(AppDbContext context, IApiFootballClient client) : base(context, client)
+        {
+        }
+
+        public async Task SeedBetsByLeagueId(int extLeagueId)
+        {
+            if (! await _context.Bets.AnyAsync())
+            {
+                var bets = await _client.DownloadAllBetsByLeagueId(extLeagueId);
+                await _context.Bets.AddRangeAsync(bets);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
