29f726a00e9fc2945d7dfed6111fc96e27124a8e
Marcin <ziewa91@wp.pl>
2021-08-24 17:51:04 +0200

    Commented unused code

--- Application/Common/Mappings/Mappers/CouponToReadCouponMapper.cs
+++ Application/Common/Mappings/Mappers/CouponToReadCouponMapper.cs
@@ -25,35 +25,36 @@ namespace Application.Common.Mappings.Mappers
             {
                 var betValue = couponBetValue.BetValue;
                 var bet = betValue.Bet;
-                var odd = bet.Odd;
+                //var odd = bet.Odd;
 
-                content.Bets.Add(new ReadCouponBetDto
-                {
-                    BetId = bet.Id,
-                    BetValueId = betValue.Id,
-                    OddId = odd.Id,
-                    Odd = betValue.Odd,
-                    OddValue = betValue.Value,
-                    LabelId = betValue.Bet.LabelId,
-                    LabelName = betValue.Bet.Label.Name,
-                    RoundId = betValue.Bet.Odd.Fixture.RoundId,
-                    RoundName = betValue.Bet.Odd.Fixture.Round.Name,
-                    LeagueId = betValue.Bet.Odd.LeagueId,
-                    LeagueName = betValue.Bet.Odd.League.Name,
-                    FixtureId = betValue.Bet.Odd.FixtureId,
-                    HomeTeam = new ReadCouponFixtureDto
-                    {
-                        TeamId = betValue.Bet.Odd.Fixture.HomeTeamId,
-                        TeamName = betValue.Bet.Odd.Fixture.HomeTeam.Name,
-                        TeamCode = betValue.Bet.Odd.Fixture.HomeTeam.Code
-                    },
-                    AwayTeam = new ReadCouponFixtureDto
-                    {
-                        TeamId = betValue.Bet.Odd.Fixture.AwayTeamId,
-                        TeamName = betValue.Bet.Odd.Fixture.AwayTeam.Name,
-                        TeamCode = betValue.Bet.Odd.Fixture.AwayTeam.Code
-                    },
-                });
+                //    content.Bets.Add(new ReadCouponBetDto
+                //    {
+                //        BetId = bet.Id,
+                //        BetValueId = betValue.Id,
+                //        OddId = odd.Id,
+                //        Odd = betValue.Odd,
+                //        OddValue = betValue.Value,
+                //        LabelId = betValue.Bet.LabelId,
+                //        LabelName = betValue.Bet.Label.Name,
+                //        RoundId = betValue.Bet.Odd.Fixture.RoundId,
+                //        RoundName = betValue.Bet.Odd.Fixture.Round.Name,
+                //        LeagueId = betValue.Bet.Odd.LeagueId,
+                //        LeagueName = betValue.Bet.Odd.League.Name,
+                //        FixtureId = betValue.Bet.Odd.FixtureId,
+                //        HomeTeam = new ReadCouponFixtureDto
+                //        {
+                //            TeamId = betValue.Bet.Odd.Fixture.HomeTeamId,
+                //            TeamName = betValue.Bet.Odd.Fixture.HomeTeam.Name,
+                //            TeamCode = betValue.Bet.Odd.Fixture.HomeTeam.Code
+                //        },
+                //        AwayTeam = new ReadCouponFixtureDto
+                //        {
+                //            TeamId = betValue.Bet.Odd.Fixture.AwayTeamId,
+                //            TeamName = betValue.Bet.Odd.Fixture.AwayTeam.Name,
+                //            TeamCode = betValue.Bet.Odd.Fixture.AwayTeam.Code
+                //        },
+                //    });
+                //}
             }
 
             return readCoupon;
