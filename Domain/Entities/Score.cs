3610869bb269a86289cf3c0744abf2064be5a7eb
Marcin <ziewa91@wp.pl>
2021-11-01 20:12:02 +0100

    Added Equals method

--- Domain/Entities/Score.cs
+++ Domain/Entities/Score.cs
@@ -1,9 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Domain.Entities
 {
-    public class Score
+    public class Score : IEquatable<Score>
     {
         [Key]
         public int Id { get; set; }
@@ -39,5 +40,37 @@ namespace Domain.Entities
 
         [ForeignKey("WinnerId")]
         public virtual Team Winner { get; set; }
+
+        public bool Equals(Score other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return ExtFixtureId == other.ExtFixtureId && GoalsHomeTeam == other.GoalsHomeTeam && GoalsAwayTeam == other.GoalsAwayTeam && HalftimeHomeGoals == other.HalftimeHomeGoals && HalftimeAwayGoals == other.HalftimeAwayGoals && FullTimeHomeGoals == other.FullTimeHomeGoals && FullTimeAwayGoals == other.FullTimeAwayGoals && ExtraTimeHomeGoals == other.ExtraTimeHomeGoals && ExtraTimeAwayGoals == other.ExtraTimeAwayGoals && PenaltyHomeGoals == other.PenaltyHomeGoals && PenaltyAwayGoals == other.PenaltyAwayGoals;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Score) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = new HashCode();
+            hashCode.Add(ExtFixtureId);
+            hashCode.Add(GoalsHomeTeam);
+            hashCode.Add(GoalsAwayTeam);
+            hashCode.Add(HalftimeHomeGoals);
+            hashCode.Add(HalftimeAwayGoals);
+            hashCode.Add(FullTimeHomeGoals);
+            hashCode.Add(FullTimeAwayGoals);
+            hashCode.Add(ExtraTimeHomeGoals);
+            hashCode.Add(ExtraTimeAwayGoals);
+            hashCode.Add(PenaltyHomeGoals);
+            hashCode.Add(PenaltyAwayGoals);
+            return hashCode.ToHashCode();
+        }
     }
 }
