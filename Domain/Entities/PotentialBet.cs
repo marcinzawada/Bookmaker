69b334bd9fa94d877ee1be21f78b1596f0547bcb
Marcin <ziewa91@wp.pl>
2021-11-04 17:58:08 +0100

    Added Equals method

--- Domain/Entities/PotentialBet.cs
+++ Domain/Entities/PotentialBet.cs
@@ -5,7 +5,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Domain.Entities
 {
-    public class PotentialBet
+    public class PotentialBet : IEquatable<PotentialBet>
     {
         [Key]
         public int Id { get; set; }
@@ -31,5 +31,25 @@ namespace Domain.Entities
         public virtual Label Label { get; set; }
 
         public virtual ICollection<BetValue> BetValues { get; set; } = new List<BetValue>();
+
+        public bool Equals(PotentialBet other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return LeagueId == other.LeagueId && FixtureId == other.FixtureId && BookieId == other.BookieId && LabelId == other.LabelId;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((PotentialBet) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(LeagueId, FixtureId, BookieId, LabelId);
+        }
     }
 }
