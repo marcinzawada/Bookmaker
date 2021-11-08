a1ff22eee5204b5169215741a851f51cc1c7d43e
Marcin <ziewa91@wp.pl>
2021-11-08 22:01:05 +0100

    Added BetTypeChecker

---
+++ Infrastructure/BackgroundJobs/CouponCheckers/BetTypesCheckers/BetTypeChecker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+using Infrastructure.Data;
+
+namespace Infrastructure.BackgroundJobs.CouponCheckers.BetTypesCheckers
+{
+    public abstract class BetTypeChecker
+    {
+        protected readonly AppDbContext _context;
+
+        protected BetTypeChecker(AppDbContext appDbContext)
+        {
+            _context = appDbContext;
+        }
+
+        public abstract Task<bool> Check(BetValue betValue);
+
+        public abstract Task<bool> CheckIsMatchAlreadyFinished(BetValue betValue);
+    }
+}
