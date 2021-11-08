b68b898f4028217cae214a222db10371bb340df4
Marcin <ziewa91@wp.pl>
2021-11-08 21:56:18 +0100

    Added BetLabel enum

---
+++ Domain/Enums/BetLabel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Enums
+{
+    public enum BetLabel
+    {
+        [Description("Match Winner")]
+        MATCH_WINNER
+    }
+}
