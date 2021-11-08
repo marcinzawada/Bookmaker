84592944e180c0f65b7e347a7d4a01e223c70835
Marcin <ziewa91@wp.pl>
2021-11-08 22:01:20 +0100

    Added BetTypeCheckerFactory

---
+++ Infrastructure/BackgroundJobs/CouponCheckers/BetTypesCheckers/BetTypeCheckerFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Domain.Entities;
+using Infrastructure.Data;
+
+namespace Infrastructure.BackgroundJobs.CouponCheckers.BetTypesCheckers
+{
+    public class BetTypeCheckerFactory
+    {
+        public BetTypeChecker CreateBetChecker(Label label, AppDbContext context)
+        {
+            switch (label.Name)
+            {
+                case "Match Winner":
+                    return new MatchWinnerChecker(context);
+                default:
+                    throw new Exception($"No BetTypeCheckerFor {label.Name}");
+            }
+        }
+    }
+}
