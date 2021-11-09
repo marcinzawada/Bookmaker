8415cf63379ac0dee0a3584853b62d05bccf924c
Marcin <ziewa91@wp.pl>
2021-11-09 22:48:55 +0100

    Added UserDto

---
+++ Application/Dto/UserDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Common.Mappings;
+using Domain.Entities;
+
+namespace Application.Dto
+{
+    public class UserDto : IMapFrom<User>
+    {
+        public int Id { get; set; }
+
+        public string Email { get; set; }
+
+        public decimal GameTokens { get; set; }
+    }
+}
