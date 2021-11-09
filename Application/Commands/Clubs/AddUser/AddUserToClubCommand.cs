84fb00638c1142c1e202661eee4dee6f71d5f2af
Marcin <ziewa91@wp.pl>
2021-11-09 22:44:43 +0100

    Added AddUserToClubCommand

---
+++ Application/Commands/Clubs/AddUser/AddUserToClubCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using Application.Commands.Clubs.Create;
+using Application.Common.Interfaces;
+using Application.Models;
+using FluentValidation;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Commands.Clubs.AddUser
+{
+
+    public record AddUserToClubCommand(string UserEmail, int ClubId) : IRequest<Response>;
+
+    public class AddUserToClubCommandValidator : AbstractValidator<AddUserToClubCommand>
+    {
+        public AddUserToClubCommandValidator(IDbContext dbContext)
+        {
+            RuleFor(x => x.UserEmail)
+                .NotNull().NotEmpty().EmailAddress();
+
+            RuleFor(x => x.ClubId)
+                .NotNull().NotEmpty().GreaterThanOrEqualTo(1);
+
+        }
+    }
+
+
+}
