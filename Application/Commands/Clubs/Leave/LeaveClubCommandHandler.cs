0a1cae3a2d3bf0321f641a4712a5b1cfae04a3da
Marcin <ziewa91@wp.pl>
2021-11-09 22:47:01 +0100

    Added LeaveClubCommandHandler

---
+++ Application/Commands/Clubs/Leave/LeaveClubCommandHandler.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Constants;
+using Application.Common.Interfaces;
+using Application.Models;
+using Application.Services;
+using Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Commands.Clubs.Leave
+{
+
+    public class LeaveClubCommandHandler : BaseCommandHandler, IRequestHandler<LeaveClubCommand, Response>
+    {
+        public LeaveClubCommandHandler(IDbContext context, IUserContextService userContextService)
+            : base(context, userContextService)
+        {
+        }
+
+        public async Task<Response> Handle(LeaveClubCommand request, CancellationToken cancellationToken)
+        {
+            var club = await _context.Clubs
+                .Include(x => x.ClubUsers)
+                .FirstOrDefaultAsync(x => x.Id == request.ClubId, cancellationToken);
+
+            if (club == null)
+                return Response.Failure(Errors.EntityNotFound<Club>());
+
+            var userId = _userContextService.GetUserId();
+            var isAdmin = userId == club.AdminId;
+
+            if (isAdmin)
+                _context.Clubs.Remove(club);
+            else
+            {
+                var clubUser = club.ClubUsers.FirstOrDefault(x => x.UserId == userId);
+
+                if (clubUser == null)
+                    return Response.Failure(Errors.Forbidden());
+
+                _context.ClubUsers.Remove(clubUser);
+            }
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Response.Success();
+        }
+    }
+
+}
