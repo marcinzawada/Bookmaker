702a8ddd9bc98a45d50622873ae123b0a9907426
Marcin <ziewa91@wp.pl>
2021-11-09 22:51:40 +0100

    Added UserContextService

--- Application/Queries/BaseQueryHandler.cs
+++ Application/Queries/BaseQueryHandler.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Application.Common.Interfaces;
+using Application.Services;
 using AutoMapper;
 
 namespace Application.Queries
@@ -12,11 +13,14 @@ namespace Application.Queries
     {
         protected readonly IDbContext _context;
         protected readonly IMapper _mapper;
+        protected readonly IUserContextService _userContextService;
 
-        protected BaseQueryHandler(IDbContext context, IMapper mapper)
+
+        protected BaseQueryHandler(IDbContext context, IMapper mapper, IUserContextService userContextService)
         {
             _context = context;
             _mapper = mapper;
+            _userContextService = userContextService;
         }
     }
 }
