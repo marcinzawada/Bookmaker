8d66133a0725c641716b1805ad182bdf80b22fda
Marcin <ziewa91@wp.pl>
2021-11-09 22:46:04 +0100

    Added CreateClubCommand

---
+++ Application/Commands/Clubs/Create/CreateClubCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Models;
+using FluentValidation;
+using MediatR;
+
+namespace Application.Commands.Clubs.Create
+{
+
+    public record CreateClubCommand(string ClubName) : IRequest<Response>;
+
+    public class CreateClubCommandValidator : AbstractValidator<CreateClubCommand>
+    {
+        public CreateClubCommandValidator()
+        {
+            RuleFor(x => x.ClubName)
+                .NotNull().NotEmpty().MinimumLength(6).MaximumLength(64);
+        }
+    }
+
+
+}
