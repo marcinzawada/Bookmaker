876143bde0ffc7923ba2ac2eea135fff84291408
Marcin <ziewa91@wp.pl>
2021-11-09 22:50:38 +0100

    Added GetAllClubsQuery

---
+++ Application/Queries/Clubs/GetAll/GetAllClubsQuery.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Models;
+using FluentValidation;
+using MediatR;
+
+namespace Application.Queries.Clubs.GetAll
+{
+
+    public record GetAllClubsQuery() : IRequest<Response>;
+
+}
