2322d5025b60143ef5bb11ee2273867b424c4099
Marcin <ziewa91@wp.pl>
2021-11-09 23:27:37 +0100

    Added GetLeaderboardQueryHandler

---
+++ Application/Queries/Leaderboard/GetLeaderboardQueryHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Interfaces;
+using Application.Dto;
+using Application.Models;
+using Application.Services;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Queries.Leaderboard
+{
+
+    public class GetLeaderboardQueryHandler : BaseQueryHandler, IRequestHandler<GetLeaderboardQuery, Response>
+    {
+        public GetLeaderboardQueryHandler(IDbContext context, IMapper mapper, IUserContextService userContextService) 
+            : base(context, mapper, userContextService)
+        {
+        }
+
+        public async Task<Response> Handle(GetLeaderboardQuery request, CancellationToken cancellationToken)
+        {
+            var users = await _context.Users
+                .OrderByDescending(x => x.GameTokens)
+                .AsNoTracking()
+                .ProjectTo<UserDto>(_mapper.ConfigurationProvider)
+                .ToListAsync(cancellationToken);
+
+            return Result<List<UserDto>>.Create(users);
+        }
+    }
+
+}
