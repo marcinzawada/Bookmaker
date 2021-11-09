c724a73541ccbd659b624068f602c84d8dcc1089
Marcin <ziewa91@wp.pl>
2021-11-09 22:48:45 +0100

    Added ClubDto

---
+++ Application/Dto/ClubDto.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Application.Common.Mappings;
+using Domain.Entities;
+
+namespace Application.Dto
+{
+    public class ClubDto : IMapFrom<Club>
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int NumberOfMembers { get; set; }
+
+        public List<UserDto> Users { get; set; }
+    }
+}
