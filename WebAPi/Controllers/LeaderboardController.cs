dcd4e91a090e03f682b62a826f21857f88fd207d
Marcin <ziewa91@wp.pl>
2021-11-09 23:27:52 +0100

    Added LeaderboardController

---
+++ WebAPi/Controllers/LeaderboardController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Extensions;
+using Application.Queries.Leaderboard;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Authorize]
+    public class LeaderboardController : ApiControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var request = new GetLeaderboardQuery();
+
+            var response = await Mediator.Send(request);
+
+            return this.CreateResponse(response);
+        }
+    }
+}
