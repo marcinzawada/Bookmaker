7d3ba3ce1ec40d3e39a9464b9147768265b54b48
Marcin <ziewa91@wp.pl>
2021-11-10 00:07:38 +0100

    Added AddTokens endpoint

--- WebAPi/Controllers/AccountController.cs
+++ WebAPi/Controllers/AccountController.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Application.Commands;
+using Application.Commands.Account.AddTokens;
 using Application.Commands.Account.Login;
 using Application.Commands.Account.Register;
 using Application.Extensions;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebAPI.Controllers
@@ -24,6 +26,17 @@ namespace WebAPI.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login(LoginCommand command)
         {
+            var response = await Mediator.Send(command);
+
+            return this.CreateResponse(response);
+        }
+
+        [Authorize]
+        [HttpPost("addTokens")]
+        public async Task<IActionResult> AddTokens()
+        {
+            var command = new AddTokensCommand();
+
             var response = await Mediator.Send(command);
 
             return this.CreateResponse(response);
