28e1c9b062c1e58cd5a90d54be38895271f5a1e2
Marcin <ziewa91@wp.pl>
2021-11-09 22:50:07 +0100

    Added GetClubQuery

---
+++ Application/Queries/Clubs/Get/GetClubQuery.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Models;
+using FluentValidation;
+using MediatR;
+
+namespace Application.Queries.Clubs.Get
+{
+
+    public record GetClubQuery(int ClubId) : IRequest<Response>;
+
+    public class GetClubQueryValidator : AbstractValidator<GetClubQuery>
+    {
+        public GetClubQueryValidator()
+        {
+            RuleFor(x => x.ClubId)
+                .NotNull().NotEmpty().GreaterThanOrEqualTo(1);
+        }
+    }
+
+}
