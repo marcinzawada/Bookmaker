6c4b225d58d6c6c82811e7155234014740d1232a
Marcin <ziewa91@wp.pl>
2021-11-09 22:46:15 +0100

    CreateClubCommandHandler

---
+++ Application/Commands/Clubs/Create/CreateClubHandler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Interfaces;
+using Application.Models;
+using Application.Services;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.Commands.Clubs.Create
+{
+    public class AddUserToClubHandler : BaseCommandHandler, IRequestHandler<CreateClubCommand, Response>
+    {
+        public AddUserToClubHandler(IDbContext context, IUserContextService userContextService) : base(context, userContextService)
+        {
+        }
+
+        public async Task<Response> Handle(CreateClubCommand request, CancellationToken cancellationToken)
+        {
+            var userId = _userContextService.GetUserId();
+
+            var newClub = new Club
+            {
+                AdminId = userId,
+                Name = request.ClubName,
+                ClubUsers = new List<ClubUser>
+                {
+                    new()
+                    {
+                        UserId = userId
+                    }
+                }
+            };
+
+            await _context.Clubs.AddAsync(newClub, cancellationToken);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return CreatedResponse.Create($"api/clubs/{newClub.Id}");
+        }
+    }
+
+}
+
