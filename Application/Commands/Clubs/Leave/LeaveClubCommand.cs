2810c52a0365b75af658599d9331907be2996ac6
Marcin <ziewa91@wp.pl>
2021-11-09 22:46:44 +0100

    Added LeaveClubCommand

---
+++ Application/Commands/Clubs/Leave/LeaveClubCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Models;
+using FluentValidation;
+using MediatR;
+
+namespace Application.Commands.Clubs.Leave
+{
+
+    public record LeaveClubCommand(int ClubId) : IRequest<Response>;
+
+    public class LeaveClubCommandValidator : AbstractValidator<LeaveClubCommand>
+    {
+        public LeaveClubCommandValidator()
+        {
+            RuleFor(x => x.ClubId)
+                .NotEmpty().NotNull().GreaterThanOrEqualTo(1);
+        }
+    }
+
+}
