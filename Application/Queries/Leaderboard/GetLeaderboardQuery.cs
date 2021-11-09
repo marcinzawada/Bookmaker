6732e8f6aabdee195d41383752bab6fd81615aba
Marcin <ziewa91@wp.pl>
2021-11-09 23:27:28 +0100

    Added GetLeaderboardQuery

---
+++ Application/Queries/Leaderboard/GetLeaderboardQuery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Models;
+using FluentValidation;
+using MediatR;
+
+namespace Application.Queries.Leaderboard
+{
+
+    public record GetLeaderboardQuery() : IRequest<Response>;
+}
