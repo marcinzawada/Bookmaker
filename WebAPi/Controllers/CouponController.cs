7215cad87e1d98c696b7bb3eaf2dba004ad0c302
Marcin <ziewa91@wp.pl>
2021-11-22 00:45:28 +0100

    Updated toi NET 6

--- WebAPi/Controllers/CouponController.cs
+++ WebAPi/Controllers/CouponController.cs
@@ -1,14 +1,31 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Application.Commands.Coupons;
 using Application.Extensions;
 using Application.Queries.Coupons;
+using Infrastructure.BackgroundJobs.ApiFootballUpdater;
+using Infrastructure.BackgroundJobs.CouponCheckers;
+using Infrastructure.ExternalApis.ApiFootball.Client;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using YamlDotNet.Serialization.NodeDeserializers;
 
 namespace WebAPI.Controllers
 {
     public class CouponController : ApiControllerBase
     {
+        private readonly FixtureUpdater _fixtureUpdater;
+        private readonly BetsUpdater _betsUpdater;
+        private readonly CouponChecker _couponChecker;
+
+        public CouponController(FixtureUpdater fixtureUpdater, BetsUpdater betsUpdater, CouponChecker couponChecker)
+        {
+            _fixtureUpdater = fixtureUpdater;
+            _betsUpdater = betsUpdater;
+            _couponChecker = couponChecker;
+        }
+
         [Authorize]
         [HttpPost]
         public async Task<IActionResult> Post(CreateCouponCommand request)
@@ -19,12 +36,24 @@ namespace WebAPI.Controllers
         }
 
 
-        [HttpGet]
-        public async Task<IActionResult> Get([FromQuery]GetCouponQuery request)
+        [HttpGet("{couponId:int}")]
+        public async Task<IActionResult> Get(int couponId)
         {
+            var request = new GetCouponQuery(couponId);
+
             var response = await Mediator.Send(request);
 
             return this.CreateResponse(response);
+        }
+
+        [HttpGet("test")]
+        public async Task<IActionResult> Test()
+        {
+            //await _fixtureUpdater.Update();
+            //await _betsUpdater.Update();
+            await _couponChecker.Check();
+
+            return Ok();
         }
     }
 }
