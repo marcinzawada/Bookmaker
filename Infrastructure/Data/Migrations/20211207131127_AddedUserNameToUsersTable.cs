6b81a45b5f83b6fefabbf6d3603c8c65cd4a2b55
Marcin <ziewa91@wp.pl>
2021-12-07 14:43:31 +0100

    Added UserName to User entity

---
+++ Infrastructure/Data/Migrations/20211207131127_AddedUserNameToUsersTable.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migrations
+{
+    public partial class AddedUserNameToUsersTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserName",
+                table: "Users",
+                type: "nvarchar(32)",
+                maxLength: 32,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserName",
+                table: "Users");
+        }
+    }
+}
