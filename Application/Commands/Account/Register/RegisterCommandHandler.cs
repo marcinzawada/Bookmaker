eaadbfc130d884835741cfe598b006ac490d0141
Marcin <ziewa91@wp.pl>
2021-12-07 14:42:31 +0100

    Now return AuthDto

--- Application/Commands/Account/Register/RegisterCommandHandler.cs
+++ Application/Commands/Account/Register/RegisterCommandHandler.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Application.Commands.Account.Login;
 using Application.Common.Interfaces;
+using Application.Common.JWT;
 using Application.Models;
 using Domain.Entities;
 using MediatR;
@@ -16,12 +18,14 @@ namespace Application.Commands.Account.Register
     public class RegisterCommandHandler : IRequestHandler<RegisterCommand, Response>
     {
         private readonly IPasswordHasher<User> _passwordHasher;
-        private readonly IDbContext _context; 
+        private readonly IDbContext _context;
+        private readonly IJwtGenerator _jwtGenerator;
 
-        public RegisterCommandHandler(IPasswordHasher<User> passwordHasher, IDbContext context)
+        public RegisterCommandHandler(IPasswordHasher<User> passwordHasher, IDbContext context, IJwtGenerator jwtGenerator)
         {
             _passwordHasher = passwordHasher;
             _context = context;
+            _jwtGenerator = jwtGenerator;
         }
 
         public async Task<Response> Handle(RegisterCommand request, CancellationToken cancellationToken)
@@ -29,6 +33,7 @@ namespace Application.Commands.Account.Register
             var newUser = new User()
             {
                 Email = request.Email.ToLower().Trim(),
+                UserName = request.UserName.Trim()
             };
 
             var hashedPassword = _passwordHasher.HashPassword(newUser, request.Password);
@@ -37,7 +42,16 @@ namespace Application.Commands.Account.Register
             await _context.Users.AddAsync(newUser, cancellationToken);
             await _context.SaveChangesAsync(cancellationToken);
 
-            return Response.Success();
+            var token = _jwtGenerator.Generate(newUser);
+
+            var authDto = new AuthDto
+            {
+                Id = newUser.Id,
+                UserName = newUser.UserName,
+                Token = token
+            };
+
+            return Result<AuthDto>.Create(authDto);
         }
     }
 }
