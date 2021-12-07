a1617f1f35ff655d8ab128448e741e144e297220
Marcin <ziewa91@wp.pl>
2021-12-07 20:14:26 +0100

    Added ValidationException

---
+++ Application/Exceptions/ValidationException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Models;
+using FluentValidation.Results;
+
+namespace Application.Exceptions
+{
+    public sealed class ValidationException : Exception 
+    {
+        public ValidationException(List<ValidationError> validationErrors)
+            => ValidationErrors = validationErrors;
+
+        public List<ValidationError> ValidationErrors { get; }
+    }
+}
