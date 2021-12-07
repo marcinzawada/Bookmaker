a97bfbedc8bac3f6a172d4ccc0d68c15b9fa3090
Marcin <ziewa91@wp.pl>
2021-12-07 20:14:50 +0100

    Added ExceptionHandlingMiddleware

---
+++ Application/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Application.Exceptions;
+using Application.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Application.Middleware
+{
+    public class ExceptionHandlingMiddleware : IMiddleware
+    {
+        private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+
+        public ExceptionHandlingMiddleware(ILogger<ExceptionHandlingMiddleware> logger)
+            => _logger = logger;
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, e.Message);
+
+                await HandleExceptionAsync(context, e);
+            }
+        }
+
+        private static async Task HandleExceptionAsync(HttpContext httpContext, Exception exception)
+        {
+            var statusCode = GetStatusCode(exception);
+
+            var response = GetResponse(exception);
+
+            httpContext.Response.ContentType = "application/json";
+
+            httpContext.Response.StatusCode = statusCode;
+
+            await httpContext.Response.WriteAsync(JsonSerializer.Serialize(response));
+        }
+
+        private static int GetStatusCode(Exception exception) =>
+            exception switch
+            {
+                ValidationException => StatusCodes.Status422UnprocessableEntity,
+                _ => StatusCodes.Status500InternalServerError
+            };
+
+        private static object GetResponse(Exception exception)
+        {
+            object response = null;
+
+            if (exception is ValidationException validationException)
+            {
+                response = ValidationResponse.Create(validationException.ValidationErrors);
+            }
+
+            return response?? "Something went wrong";
+        }
+    }
+}
