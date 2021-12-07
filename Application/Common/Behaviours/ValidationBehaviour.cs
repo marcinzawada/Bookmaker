aec6cf6b29fe44e522dc593aab573c0feae61ca0
Marcin <ziewa91@wp.pl>
2021-12-07 20:13:32 +0100

    Added ValidationBehaviour

---
+++ Application/Common/Behaviours/ValidationBehaviour.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Models;
+using FluentValidation;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+
+namespace Application.Common.Behaviours
+{
+    public class ValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public ValidationBehaviour(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            if (!_validators.Any())
+                return await next();
+
+            var context = new ValidationContext<TRequest>(request);
+
+            var validationResults = await Task.WhenAll(_validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+
+            var validationErrors = validationResults
+                .SelectMany(x => x.Errors)
+                .Where(x => x != null)
+                .GroupBy(
+                    x => x.PropertyName,
+                    x => x.ErrorMessage,
+                    (propertyName, errorMessages) =>
+                        new ValidationError(propertyName, errorMessages.Distinct().ToList())
+                )
+                .ToList();
+
+            if (validationErrors.Any())
+            {
+                throw new Exceptions.ValidationException(validationErrors);
+            }
+
+            return await next();
+        }
+    }
+}
