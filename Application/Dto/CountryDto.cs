3ee58bb27904994adc6bede375ceb7b5cb2eedac
Marcin <ziewa91@wp.pl>
2021-12-07 14:44:18 +0100

    Added CountryDto

---
+++ Application/Dto/CountryDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Common.Mappings;
+using Domain.Entities;
+
+namespace Application.Dto
+{
+    public class CountryDto : IMapFrom<Country>
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Flag { get; set; }
+
+    }
+}
