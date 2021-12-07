84f9847c2d5cc9936c7297f67004c03b0da2b5dc
Marcin <ziewa91@wp.pl>
2021-12-07 14:40:44 +0100

    Added UserName to claims

--- Application/Common/JWT/JwtGenerator.cs
+++ Application/Common/JWT/JwtGenerator.cs
@@ -25,6 +25,7 @@ namespace Application.Common.JWT
             {
                 new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
                 new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.Name, user.UserName)
             };
 
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_authenticationSettings.JwtKey));
