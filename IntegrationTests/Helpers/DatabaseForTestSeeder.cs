2b69cb4871c258cab8ac25478d0dc6961c533b8c
Marcin <ziewa91@wp.pl>
2021-12-12 01:33:24 +0100

    Added DatabaseForTestSeeder

---
+++ IntegrationTests/Helpers/DatabaseForTestSeeder.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Domain.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Identity;
+
+namespace IntegrationTests.Helpers;
+
+public class DatabaseForTestSeeder
+{
+    private readonly AppDbContext _context;
+    private readonly PasswordHasher<User> _passwordHasher;
+
+    public DatabaseForTestSeeder(AppDbContext context, PasswordHasher<User> passwordHasher)
+    {
+        _context = context;
+        _passwordHasher = passwordHasher;
+    }
+
+    public void Seed()
+    {
+        if (_context.Users.Any())
+            return;
+
+        var users = SeedUser();
+
+
+    }
+
+    private List<User> SeedUser()
+    {
+        var users = new List<User>
+            {
+                new User
+                {
+                    Id = 1,
+                    Email = "test@gmail.com",
+                    PasswordHash = _passwordHasher.HashPassword(null!, "Pass123!"),
+                    UserName = "username"
+                },
+                new User
+                {
+                    Id = 2,
+                    Email = "test2@gmail.com",
+                    PasswordHash = _passwordHasher.HashPassword(null!, "Pass123!"),
+                    UserName = "username2"
+                },
+            };
+
+        _context.Users.AddRange(users);
+        _context.SaveChanges();
+
+        return users;
+    }
+
+
+}
+
+
