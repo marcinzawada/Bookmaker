f8d91d966fb33a2ccc4afc7bce14a96f785f7db4
Marcin <ziewa91@wp.pl>
2022-04-03 21:50:05 +0200

    Added get coupon query

---
+++ Application/Queries/Coupons/Get/GetCouponQuery.cs
@@ -0,0 +1,19 @@
+﻿using Application.Models;
+using FluentValidation;
+using MediatR;
+
+namespace Application.Queries.Coupons.Get
+{
+
+    public record GetCouponQuery(int CouponId) : IRequest<Response>;
+
+    public class RequestValidator : AbstractValidator<GetCouponQuery>
+    {
+        public RequestValidator()
+        {
+            RuleFor(x => x.CouponId)
+                .NotEmpty().NotNull().GreaterThanOrEqualTo(1);
+        }
+    }
+
+}
---
+++ Application/Queries/Coupons/Get/GetCouponQueryHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Constants;
+using Application.Common.Interfaces;
+using Application.Models;
+using Application.Services;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Queries.Coupons.Get
+{
+
+    public class GetCouponHandler : BaseQueryHandler, IRequestHandler<GetCouponQuery, Response>
+    {
+        public GetCouponHandler(IDbContext context, IMapper mapper, IUserContextService userCtx) 
+            : base(context, mapper, userCtx)
+        {
+        }
+
+        public async Task<Response> Handle(GetCouponQuery request, CancellationToken cancellationToken)
+        {
+            var readCoupon = await _context.ReadCoupons
+                .AsNoTracking()
+                .Include(x => x.Items)
+                .FirstOrDefaultAsync(x => x.CouponId == request.CouponId, cancellationToken);
+
+            var readCouponDto = _mapper.Map<ReadCouponDto>(readCoupon);
+
+            if (readCoupon == null)
+                return Response.Failure(Errors.EntityNotFound<ReadCoupon>());
+
+            return Result<ReadCouponDto>.Create(readCouponDto);
+        }
+    }
+
+}
