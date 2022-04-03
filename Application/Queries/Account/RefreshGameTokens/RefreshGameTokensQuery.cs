a5696e003e24d1f2b8c47e47ad69ca91d8a96e35
Marcin <ziewa91@wp.pl>
2022-04-03 21:52:48 +0200

    Added RefreshGameTokens query

---
+++ Application/Queries/Account/RefreshGameTokens/RefreshGameTokensQuery.cs
@@ -0,0 +1,8 @@
+﻿using Application.Models;
+using MediatR;
+
+namespace Application.Queries.Account.RefreshTokens
+{
+
+    public record AddTokensCommand() : IRequest<Response>;
+}
---
+++ Application/Queries/Account/RefreshGameTokens/RefreshGameTokensQueryHandler.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Commands;
+using Application.Common.Interfaces;
+using Application.Models;
+using Application.Services;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Queries.Account.RefreshTokens
+{
+
+    public class AddTokensCommandHandler : BaseCommandHandler, IRequestHandler<AddTokensCommand, Response>
+    {
+        public AddTokensCommandHandler(IDbContext context, IUserContextService userContextService) : base(context, userContextService)
+        {
+        }
+
+        public async Task<Response> Handle(AddTokensCommand request, CancellationToken cancellationToken)
+        {
+            var userId = _userContextService.GetUserId();
+
+            var user = await _context.Users
+                .FirstOrDefaultAsync(x => x.Id == userId, cancellationToken);
+
+            if (user == null)
+                throw new Exception($"User with id: {userId} doesn't exist in base");
+
+            var hasPassed24Hours = user.TimeOfLastTokensReceived.AddHours(24) < DateTime.UtcNow;
+
+            if (hasPassed24Hours)
+            {
+                user.GameTokens += 100;
+                user.TimeOfLastTokensReceived = DateTime.UtcNow;
+                _context.Users.Update(user);
+                await _context.SaveChangesAsync(cancellationToken);
+            }
+
+            return Result<decimal>.Create(user.GameTokens);
+        }
+    }
+
+}
