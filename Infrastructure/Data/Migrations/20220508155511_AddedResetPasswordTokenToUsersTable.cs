fa0057edb378b4a4c141ff1276271697224a5964
Marcin <ziewa91@wp.pl>
2022-05-15 00:37:56 +0200

    Added ResetPasswordToken to users table

---
+++ Infrastructure/Data/Migrations/20220508155511_AddedResetPasswordTokenToUsersTable.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migrations
+{
+    public partial class AddedResetPasswordTokenToUsersTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_LeagueTeams_Leagues_LeagueId",
+                table: "LeagueTeams");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ResetPasswordToken",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_LeagueTeams_Leagues_LeagueId",
+                table: "LeagueTeams",
+                column: "LeagueId",
+                principalTable: "Leagues",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_LeagueTeams_Leagues_LeagueId",
+                table: "LeagueTeams");
+
+            migrationBuilder.DropColumn(
+                name: "ResetPasswordToken",
+                table: "Users");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_LeagueTeams_Leagues_LeagueId",
+                table: "LeagueTeams",
+                column: "LeagueId",
+                principalTable: "Leagues",
+                principalColumn: "Id");
+        }
+    }
+}
