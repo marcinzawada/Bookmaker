b0e8f89a395469b0cf84870084c323a18b18b408
Marcin <ziewa91@wp.pl>
2022-05-15 00:25:21 +0200

    Added MatchStatus

--- Application/Dto/FixtureDto.cs
+++ Application/Dto/FixtureDto.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Application.Common.Mappings;
 using Domain.Entities;
+using Domain.Enums;
 
 namespace Application.Dto
 {
@@ -29,6 +30,8 @@ namespace Application.Dto
         public TeamDto AwayTeam { get; init; }
 
         public DateTime? EventDate { get; init; }
+
+        public MatchStatus Status { get; set; }
 
     }
 }
