c2f2d56c26d7fe79f9cdf8e6def147e32c69b39a
Marcin <ziewa91@wp.pl>
2022-05-15 00:12:40 +0200

    Added reset password and change password

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Threading.Tasks;
+using Application.Commands.Account.ChangePassword;
 using Application.Commands.Account.Login;
 using Application.Commands.Account.Register;
+using Application.Commands.Account.ResetPassword;
 using Application.Extensions;
 using Application.Queries.Account.RefreshTokens;
 using Microsoft.AspNetCore.Authorization;
@@ -27,11 +29,27 @@ namespace API.Controllers
         }
 
         [Authorize]
-        [HttpGet("refreshGameTokens")]
+        [HttpGet("game-tokens")]
         public async Task<IActionResult> RefreshGameTokens()
         {
             var command = new AddTokensCommand();
 
+            var response = await Mediator.Send(command);
+
+            return this.CreateResponse(response);
+        }
+
+        [HttpPost("password-reset")]
+        public async Task<IActionResult> ResetPassword(ResetPasswordCommand command)
+        {
+            var response = await Mediator.Send(command);
+
+            return this.CreateResponse(response);
+        }
+
+        [HttpPost("change-password")]
+        public async Task<IActionResult> ChangePassword(ChangePasswordCommand command)
+        {
             var response = await Mediator.Send(command);
 
             return this.CreateResponse(response);
