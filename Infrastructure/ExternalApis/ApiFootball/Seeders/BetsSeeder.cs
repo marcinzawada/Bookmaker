e50831aa3db46f058a9aa8b9d6fdcc171afb1069
Marcin <ziewa91@wp.pl>
2022-05-15 00:41:26 +0200

    Added seeding bets for many leagues

--- Infrastructure/ExternalApis/ApiFootball/Seeders/BetsSeeder.cs
+++ Infrastructure/ExternalApis/ApiFootball/Seeders/BetsSeeder.cs
@@ -1,24 +1,40 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Infrastructure.Data;
 using Infrastructure.ExternalApis.ApiFootball.Client;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace Infrastructure.ExternalApis.ApiFootball.Seeders
 {
     public class BetsSeeder : BaseSeeder
     {
-        public BetsSeeder(AppDbContext context, IApiFootballClient client) : base(context, client)
+        private readonly ILogger<BetsSeeder> _logger;
+
+        public BetsSeeder(AppDbContext context, IApiFootballClient client, ILogger<BetsSeeder> logger) : base(context, client)
         {
+            _logger = logger;
         }
 
-        public async Task SeedBetsByLeagueId(int extLeagueId)
+        public async Task SeedBetsByLeagueId(IEnumerable<int> leagueIds)
         {
-            if (! await _context.PotentialBets.AnyAsync())
+            foreach (var leagueId in leagueIds)
             {
-                var bets = await _client.DownloadAllBetsByLeagueId(extLeagueId);
+                var league = await _context.Leagues.FirstOrDefaultAsync(x => x.ExtLeagueId == leagueId);
+
+                if (_context.PotentialBets.Any(x => x.LeagueId == league.Id)) 
+                    continue;
+
+                var bets = await _client.DownloadAllBetsByLeagueId(leagueId);
+                if (bets == null)
+                    continue;
+
                 await _context.PotentialBets.AddRangeAsync(bets);
-                await _context.SaveChangesAsync();
+                _logger.LogInformation($"Added {bets.Count} new bets. ExtLeagueId: {leagueId}");
             }
+            await _context.SaveChangesAsync();
+
         }
     }
 }
