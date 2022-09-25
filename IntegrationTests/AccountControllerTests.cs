d26aec7d4610a545e85588f801b5fcedb3ce861e
Marcin <ziewa91@wp.pl>
2022-09-26 01:17:48 +0200

    Cleaning

--- IntegrationTests/AccountControllerTests.cs
+++ IntegrationTests/AccountControllerTests.cs
@@ -1,72 +1,17 @@
-using System.Collections.Generic;
+using Application.Commands.Account;
+using Application.Commands.Account.Login;
+using Application.Models;
+using FluentAssertions;
 using System.Net;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
-using Application.Commands.Account;
-using Application.Commands.Account.Login;
-using Application.Commands.Account.Register;
-using Application.Models;
 using Xunit;
-using FluentAssertions;
 
 
 namespace IntegrationTests
 {
     public class AccountControllerTests : BaseIntegrationTest
     {
-        //[Fact]
-        //public async Task Register_WithCorrectData_ShouldReturnOkStatus()
-        //{
-        //    //Arrange
-
-        //    //Act
-        //    var response = await _testClient.PostAsJsonAsync("/api/account/register",
-        //        new RegisterCommand(
-        //        _faker.Internet.Email(),
-        //        "TestUser",
-        //        "Pass123!"));
-
-
-
-        //    //Assert
-        //    response.StatusCode.Should().Be(HttpStatusCode.OK);
-        //}
-
-        //public static IEnumerable<object[]> IncorrectRegisterData =>
-        //    new List<object[]>
-        //    {
-        //        new object[] { "test@gmail.com", _faker.Person.UserName, "Pass123!"},
-        //        new object[] { "", _faker.Person.UserName, "Pass123!" },
-        //        new object[] { "test", _faker.Person.UserName, "Pass123!"},
-        //        new object[] { _faker.Internet.Email(), "" , "Pass123!" },
-        //        new object[] { _faker.Internet.Email(), _faker.Person.UserName, "", },
-        //        //new object[] { _faker.Internet.Email(), _faker.Person.UserName, "Pass123" },
-        //        //new object[] { _faker.Internet.Email(), _faker.Person.UserName, "Pa1!"},
-        //        //new object[] { _faker.Internet.Email(), _faker.Person.UserName, "pass123!"},
-        //        //new object[] { _faker.Internet.Email(), _faker.Person.UserName, "password!"},
-        //        //new object[] { _faker.Internet.Email(), _faker.Person.UserName, "password123"},
-        //        //new object[] { _faker.Internet.Email(), _faker.Person.UserName, "PASSWORD123!" },
-        //        ////new object[] { _faker.Internet.Email(), _faker.Person.UserName, "Pass 123!"},
-        //        //new object[] { _faker.Internet.Email(), _faker.Person.UserName, "Pass#123!" },
-        //        //new object[] { _faker.Internet.Email(), "username", "Pass#123!"},
-        //        new object[] { _faker.Internet.Email(), "a", "Pass123!"},
-        //        new object[] { _faker.Internet.Email(), "tooooooooooooooooooo long username", "Pass123!" },
-        //    };
-
-        //[Theory]
-        //[MemberData(nameof(IncorrectRegisterData))]
-        //public async Task Register_WithIncorrectData_ShouldReturnBadRequest(string email, string name, string password)
-        //{
-        //    //Arrange
-
-        //    //Act
-        //    var response = await _testClient.PostAsJsonAsync("/api/account/register",
-        //        new RegisterCommand(email, name, password));
-
-        //    //Assert
-        //    response.StatusCode.Should().Be(HttpStatusCode.UnprocessableEntity);
-        //}
-
         [Fact]
         public async Task Login_WithCorrectCredentials_ShouldReturnOkStatus()
         {
