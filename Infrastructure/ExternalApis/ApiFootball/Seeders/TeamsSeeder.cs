25432e06ced978926cae4eef213e13957ab08bc2
Marcin <ziewa91@wp.pl>
2022-09-26 01:15:10 +0200

    Fixed logic in loop

--- Infrastructure/ExternalApis/ApiFootball/Seeders/TeamsSeeder.cs
+++ Infrastructure/ExternalApis/ApiFootball/Seeders/TeamsSeeder.cs
@@ -1,41 +1,35 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Domain.Entities;
 using Infrastructure.Data;
 using Infrastructure.ExternalApis.ApiFootball.Client;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace Infrastructure.ExternalApis.ApiFootball.Seeders
+namespace Infrastructure.ExternalApis.ApiFootball.Seeders;
+
+public class TeamsSeeder : BaseSeeder
 {
-    public class TeamsSeeder : BaseSeeder
+    public TeamsSeeder(AppDbContext context, IApiFootballClient client) : base(context, client)
     {
-        public TeamsSeeder(AppDbContext context, IApiFootballClient client) : base(context, client)
+    }
+
+    public async Task SeedTeamsByExtLeagueId(IEnumerable<int> leagueIds)
+    {
+        var teams = new List<Team>();
+
+        foreach (var leagueId in leagueIds)
         {
+            var league = await _context.Leagues.FirstOrDefaultAsync(x => x.ExtLeagueId == leagueId);
+
+            if (_context.LeagueTeams.Any(x => x.LeagueId == league.Id))
+                continue;
+
+            var teamsToAdd = await _client.DownloadTeamsByLeagueId(leagueId);
+            teams.AddRange(teamsToAdd);
         }
 
-        public async Task SeedTeamsByExtLeagueId(IEnumerable<int> leagueIds)
-        {
-            foreach (var leagueId in leagueIds)
-            {
-                var league = await _context.Leagues.FirstOrDefaultAsync(x => x.ExtLeagueId == leagueId);
-
-                if (_context.Teams.Any(x => x.Leagues.Any(y => y.LeagueId == league.Id))) 
-                    continue;
-
-                var teams = await _client.DownloadTeamsByLeagueId(leagueId);
-                await _context.Teams.AddRangeAsync(teams);
-            }
-
-            await _context.SaveChangesAsync();
-
-            //var league = await _context.Leagues.FirstOrDefaultAsync(x => x.ExtLeagueId == id);
-
-            //if (_context.Teams.Count(x => x.Leagues.Any(y => y.LeagueId == league.Id)) == 0)
-            //{
-            //    var teams = await _client.DownloadTeamsByLeagueId(id);
-            //    await _context.Teams.AddRangeAsync(teams);
-            //    await _context.SaveChangesAsync();
-            //}
-        }
+        await _context.Teams.AddRangeAsync(teams);
+        await _context.SaveChangesAsync();
     }
 }
