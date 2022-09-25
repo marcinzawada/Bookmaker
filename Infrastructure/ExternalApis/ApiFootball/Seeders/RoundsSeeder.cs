572b4992bf188012a2691e5f71459b0461e6b9fa
Marcin <ziewa91@wp.pl>
2022-09-26 01:15:50 +0200

    Added continue to loop when league is null

--- Infrastructure/ExternalApis/ApiFootball/Seeders/RoundsSeeder.cs
+++ Infrastructure/ExternalApis/ApiFootball/Seeders/RoundsSeeder.cs
@@ -1,9 +1,9 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Infrastructure.Data;
+﻿using Infrastructure.Data;
 using Infrastructure.ExternalApis.ApiFootball.Client;
 using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Infrastructure.ExternalApis.ApiFootball.Seeders
 {
@@ -23,7 +23,10 @@ namespace Infrastructure.ExternalApis.ApiFootball.Seeders
                 var league = _context.Leagues.FirstOrDefault(x => x.ExtLeagueId == leagueId);
 
                 if (league == null)
+                {
                     _logger.LogError($"League not found. ExtLeagueId: {leagueId}");
+                    continue;
+                }
 
                 if (_context.Rounds.Any(x => x.LeagueId == league.Id))
                     continue;
@@ -33,18 +36,6 @@ namespace Infrastructure.ExternalApis.ApiFootball.Seeders
             }
             await _context.SaveChangesAsync();
 
-
-            //var league = _context.Leagues.FirstOrDefault(x => x.ExtLeagueId == id);
-
-            //if (league == null)
-            //    _logger.LogError($"League not found. ExtLeagueId: {id}");
-
-            //if (!_context.Rounds.Any(x => x.LeagueId == league.Id))
-            //{
-            //    var rounds = await _client.DownloadAllRoundsByLeagueId(id);
-            //    await _context.Rounds.AddRangeAsync(rounds);
-            //    await _context.SaveChangesAsync();
-            //}
         }
     }
 }
